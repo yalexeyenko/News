4d411ef3369e1401ad61c190449a1a7bc3a005f8
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-06-22 21:17:02 +0600

    Create addNews(), showViewNews(), deleteNews(), showEditNews(), editNews() methods.

--- src/main/java/com/epam/yalexeyenko/action/NewsAction.java
+++ src/main/java/com/epam/yalexeyenko/action/NewsAction.java
@@ -7,6 +7,7 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.actions.DispatchAction;
+import org.joda.time.LocalDate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,7 +31,82 @@ public class NewsAction extends DispatchAction {
 	public ActionForward showAddNews(ActionMapping mapping, ActionForm form, HttpServletRequest request,
 			HttpServletResponse response) {
 		log.debug("showAddNews()...");
+		NewsForm newsForm = (NewsForm) form;
+		newsForm.setDate(new LocalDate().toString());
 		return mapping.findForward("showAddNews");
 	}
 	
+	public ActionForward addNews(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		log.debug("addNews()...");
+		NewsForm newsForm = (NewsForm) form;
+		News news = new News();
+		news.setNewsTitle(newsForm.getNewsTitle());
+		news.setDate(new LocalDate(newsForm.getDate()));
+		news.setBrief(newsForm.getBrief());
+		news.setContent(newsForm.getContent());
+		try (NewsService newsService = new NewsService()) {
+			news = newsService.createNews(news);
+		}
+		return mapping.findForward("showAddNews");
+	}
+	
+	public ActionForward showViewNews(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		log.debug("showViewNews()...");
+		NewsForm newsForm = (NewsForm) form;
+		try (NewsService newsService = new NewsService()) {
+			newsForm.setNews(newsService.findNewsById(Integer.valueOf(newsForm.getId())));
+		}
+		return mapping.findForward("showViewNews");
+	}
+	
+	public ActionForward deleteNews(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		log.debug("deleteNews()...");
+		NewsForm newsForm = (NewsForm) form;
+		try (NewsService newsService = new NewsService()) {
+			newsService.deleteNewsById(Integer.valueOf(newsForm.getId()));
+		}
+		return mapping.findForward("deleteNews");
+	}
+	
+	public ActionForward showEditNews(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		log.debug("showEditNews()...");
+		NewsForm newsForm = (NewsForm) form;
+		try (NewsService newsService = new NewsService()) {
+			News news = newsService.findNewsById(Integer.valueOf(newsForm.getId()));
+			newsForm.setNews(news);
+			String id = String.valueOf(news.getId());
+			log.debug("id={}", id);
+			newsForm.setId(id);
+		}
+		return mapping.findForward("showEditNews");
+	}
+	
+	public ActionForward editNews(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		log.debug("editNews()...");		
+		NewsForm newsForm = (NewsForm) form;
+		News news = new News();
+		log.debug("id={}", newsForm.getId());
+		news.setId(Integer.parseInt(newsForm.getId()));
+		news.setNewsTitle(newsForm.getNewsTitle());
+		news.setDate(new LocalDate(newsForm.getDate()));
+		news.setBrief(newsForm.getBrief());
+		news.setContent(newsForm.getContent());
+		newsForm.setNews(news);
+		
+		log.debug("newsTitle={}", newsForm.getNewsTitle());
+		log.debug("date={}", newsForm.getDate());
+		log.debug("brief={}", newsForm.getBrief());
+		log.debug("content={}", newsForm.getContent());	
+		log.debug("!!!!!!!!!");
+		
+		try (NewsService newsService = new NewsService()) {
+			newsService.updateNews(news);
+		}
+		return mapping.findForward("showViewNews");
+	}
 }
