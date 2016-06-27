9753be3915c17be73ac96a394ad85455084bdb70
Yevgeniy_Alexeyenko <Yevgeniy_Alexeyenko@EPKZKARW0446.moscow.epam.com>
2016-06-27 14:10:34 +0600

    Move repeatable values to constants. Move PreparedStatement and Statement to try-with-resources block. Organize imports.

--- src/main/java/com/epam/yalexeyenko/dao/NewsDaoImpl.java
+++ src/main/java/com/epam/yalexeyenko/dao/NewsDaoImpl.java
@@ -5,10 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,6 +21,12 @@ public class NewsDaoImpl implements NewsDao {
 	private static final String FIND_ALL_NEWS = "SELECT * FROM NEWSBLOCK ORDER BY NEWSDATE DESC";
 	private static final String UPDATE_NEWS = "UPDATE NEWSBLOCK SET TITLE = ?, NEWSDATE = ?, BRIEF = ?, NEWSCONTENT = ? WHERE ID = ?";
 	private static final String DELETE_NEWS_BY_ID = "DELETE FROM NEWSBLOCK WHERE ID = ?";
+	
+	private static final String ID = "ID";
+	private static final String TITLE = "TITLE";
+	private static final String NEWSDATE = "NEWSDATE";
+	private static final String BRIEF = "BRIEF";
+	private static final String NEWSCONTENT = "NEWSCONTENT";
 
 	private final Connection connection;
 
@@ -33,9 +37,7 @@ public class NewsDaoImpl implements NewsDao {
 	@Override
 	public News insert(News news) {
 		log.debug("insert()...");
-		PreparedStatement preparedStatement = null;
-		try {
-			preparedStatement = connection.prepareStatement(INSERT_NEWS, new String[]{"ID"});
+		try (PreparedStatement preparedStatement = connection.prepareStatement(INSERT_NEWS, new String[]{ID});) {
 			preparedStatement.setString(1, news.getNewsTitle());
 			preparedStatement.setDate(2, new java.sql.Date(news.getDate().getTime()));
 			preparedStatement.setString(3, news.getBrief());
@@ -46,16 +48,7 @@ public class NewsDaoImpl implements NewsDao {
 			news.setId(resultSet.getInt(1));
 			return news;
 		} catch (SQLException e) {
-			e.printStackTrace();
 			throw new DaoException("SQL INSERT_NEWS error.", e);
-		} finally {
-			if (preparedStatement != null) {
-				try {
-					preparedStatement.close();
-				} catch (SQLException e) {
-					throw new DaoException("Failed to close PreparedStatement", e);
-				}
-			}
 		}
 	}
 
@@ -63,66 +56,44 @@ public class NewsDaoImpl implements NewsDao {
 	public News findById(int id) {
 		log.debug("findById()...");
 		News news = new News();
-		PreparedStatement preparedStatement = null;
-		try {
-			preparedStatement = connection.prepareStatement(FIND_NEWS_BY_ID);
+		try (PreparedStatement preparedStatement = connection.prepareStatement(FIND_NEWS_BY_ID);) {
 			preparedStatement.setInt(1, id);
 			ResultSet resultSet = preparedStatement.executeQuery();
 			resultSet.next();
-			news.setId(resultSet.getInt("ID"));
-			news.setNewsTitle(resultSet.getString("TITLE"));
-			news.setDate(new java.util.Date((resultSet.getDate("NEWSDATE").getTime())));
-			news.setBrief(resultSet.getString("BRIEF"));
-			news.setContent(resultSet.getString("NEWSCONTENT"));
+			news.setId(resultSet.getInt(ID));
+			news.setNewsTitle(resultSet.getString(TITLE));
+			news.setDate(new java.util.Date((resultSet.getDate(NEWSDATE).getTime())));
+			news.setBrief(resultSet.getString(BRIEF));
+			news.setContent(resultSet.getString(NEWSCONTENT));
 			return news;
 		} catch (SQLException e) {
 			throw new DaoException("SQL FIND_NEWS_BY_ID error.", e);
-		} finally {
-			if (preparedStatement != null) {
-				try {
-					preparedStatement.close();
-				} catch (SQLException e) {
-					throw new DaoException("Failed to close PreparedStatement", e);
-				}
-			}
 		}
 	}
 
 	@Override
 	public List<News> findAll() {
 		List<News> newsList = new ArrayList<>();
-		Statement statement = null;
-		try {
-			statement = connection.createStatement();
+		try (Statement statement = connection.createStatement()) {
 			ResultSet resultSet = statement.executeQuery(FIND_ALL_NEWS);
 			while (resultSet.next()) {
 				News news = new News();
-				news.setId(resultSet.getInt("ID"));
-				news.setNewsTitle(resultSet.getString("TITLE"));
-				news.setDate(new java.util.Date((resultSet.getDate("NEWSDATE").getTime())));
-				news.setBrief(resultSet.getString("BRIEF"));
-				news.setContent(resultSet.getString("NEWSCONTENT"));
+				news.setId(resultSet.getInt(ID));
+				news.setNewsTitle(resultSet.getString(TITLE));
+				news.setDate(new java.util.Date((resultSet.getDate(NEWSDATE).getTime())));
+				news.setBrief(resultSet.getString(BRIEF));
+				news.setContent(resultSet.getString(NEWSCONTENT));
 				newsList.add(news);
 			}
 			return newsList;
 		} catch (SQLException e) {
 			throw new DaoException("SQL FIND_ALL_NEWS error.", e);
-		} finally {
-			if (statement != null) {
-				try {
-					statement.close();
-				} catch (SQLException e) {
-					throw new DaoException("Failed to close Statement", e);
-				}
-			}
 		}
 	}
 
 	@Override
 	public void update(News news) {
-		PreparedStatement preparedStatement = null;
-		try {
-			preparedStatement = connection.prepareStatement(UPDATE_NEWS);
+		try (PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_NEWS)) {
 			preparedStatement.setString(1, news.getNewsTitle());
 			preparedStatement.setDate(2, new java.sql.Date(news.getDate().getTime()));
 			preparedStatement.setString(3, news.getBrief());
@@ -130,36 +101,17 @@ public class NewsDaoImpl implements NewsDao {
 			preparedStatement.setInt(5, news.getId());
 			preparedStatement.executeUpdate();
 		} catch (SQLException e) {
-			e.printStackTrace();
             throw new DaoException("SQL UPDATE_NEWS error.", e);
-        } finally {
-            if (preparedStatement != null) {
-                try {
-                    preparedStatement.close();
-                } catch (SQLException e) {
-                    throw new DaoException("Failed to close PreparedStatement", e);
-                }
-            }
         }
 	}
 
 	@Override
 	public boolean delete(int id) {
-		PreparedStatement preparedStatement = null;
-		try {
-			preparedStatement = connection.prepareStatement(DELETE_NEWS_BY_ID);
+		try (PreparedStatement preparedStatement = connection.prepareStatement(DELETE_NEWS_BY_ID)) {
 			preparedStatement.setInt(1, id);
 			return (preparedStatement.executeUpdate() != 0);
 		} catch (SQLException e) {
             throw new DaoException("SQL DELETE_NEWS_BY_ID error.", e);
-        } finally {
-            if (preparedStatement != null) {
-                try {
-                    preparedStatement.close();
-                } catch (SQLException e) {
-                    throw new DaoException("Failed to close PreparedStatement", e);
-                }
-            }
         }
 	}
 	
