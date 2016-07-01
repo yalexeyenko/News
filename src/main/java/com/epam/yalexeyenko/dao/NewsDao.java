d1ff05dee22646c4e61829c9068d49dbc9616e01
Yevgeniy_Alexeyenko <Yevgeniy_Alexeyenko@.epam.com>
2016-07-01 14:30:46 +0600

    Create findAllOrderByDate() method.

--- src/main/java/com/epam/yalexeyenko/dao/NewsDao.java
+++ src/main/java/com/epam/yalexeyenko/dao/NewsDao.java
@@ -1,7 +1,9 @@
 package com.epam.yalexeyenko.dao;
 
+import java.util.List;
+
 import com.epam.yalexeyenko.model.News;
 
 public interface NewsDao extends Dao<News> {
-	
+	List<News> findAllOrderByDate();
 }
