755aec5776e3b0a8c73c9f45d076a52be0de1a43
Yevgeniy_Alexeyenko <Yevgeniy_Alexeyenko@.epam.com>
2016-07-05 18:01:04 +0600

    Delete - Delete jdbcDaoFactory field, constructor.

--- src/main/java/com/epam/yalexeyenko/service/NewsService.java
+++ src/main/java/com/epam/yalexeyenko/service/NewsService.java
@@ -5,19 +5,20 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.epam.yalexeyenko.dao.DaoFactory;
 import com.epam.yalexeyenko.dao.NewsDao;
 import com.epam.yalexeyenko.model.News;
 
-public class NewsService implements AutoCloseable {
+public class NewsService {
 	private static final Logger log = LoggerFactory.getLogger(NewsService.class);
 
-	private DaoFactory jdbcDaoFactory;
 	private NewsDao newsDao;
 
-	public NewsService() {
-		jdbcDaoFactory = DaoFactory.newInstance(DaoFactory.HIBER);
-		newsDao = (NewsDao) jdbcDaoFactory.createDao(News.class);
+	public NewsDao getNewsDao() {
+		return newsDao;
+	}
+
+	public void setNewsDao(NewsDao newsDao) {
+		this.newsDao = newsDao;
 	}
 
 	public News createNews(News news) {
@@ -42,13 +43,5 @@ public class NewsService implements AutoCloseable {
 	
 	public void deleteNewsById(int id) {
 		newsDao.delete(id);
-	}
-	
-	@Override
-	public void close() throws Exception {
-		if (jdbcDaoFactory != null) {
-			jdbcDaoFactory.close();
-		}
-		
 	}
 }
