7dc9943b2aa4526345a65ca924e6b9e8fa3b5569
Yevgeniy_Alexeyenko <Yevgeniy_Alexeyenko@.epam.com>
2016-07-14 17:50:44 +0600

    Add - Create NewsCheckbox class.

---
+++ src/main/java/com/epam/yalexeyenko/util/NewsCheckbox.java
@@ -0,0 +1,19 @@
+package com.epam.yalexeyenko.util;
+
+import java.util.List;
+
+public class NewsCheckbox {
+	private List<Integer> idList;
+
+	public NewsCheckbox() {
+	}
+
+	public List<Integer> getIdList() {
+		return idList;
+	}
+
+	public void setIdList(List<Integer> idList) {
+		this.idList = idList;
+	}
+	
+}
