c5c7696b205b29924ae5552b4630860dad94fdc3
Yevgeniy_Alexeyenko <Yevgeniy_Alexeyenko@.epam.com>
2016-07-15 17:58:45 +0600

    Add - Create class.

--- src/main/java/com/epam/yalexeyenko/config/AppConfiguration.java
+++ src/main/java/com/epam/yalexeyenko/config/AppConfiguration.java
@@ -1,22 +1,40 @@
 package com.epam.yalexeyenko.config;
 
 import java.util.Locale;
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+@EnableWebMvc
 @Configuration
 @ComponentScan("com.epam.yalexeyenko")
+@EnableTransactionManagement
+@EnableJpaRepositories
 public class AppConfiguration extends WebMvcConfigurerAdapter {
+
+	/* i18n */
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
@@ -36,17 +54,58 @@ public class AppConfiguration extends WebMvcConfigurerAdapter {
 	@Bean
 	public LocaleResolver localeResolver() {
 		CookieLocaleResolver resolver = new CookieLocaleResolver();
-		resolver.setDefaultLocale(Locale.ENGLISH);
+		resolver.setDefaultLocale(new Locale("en"));
 		resolver.setCookieName("LocaleCookie");
 		resolver.setCookieMaxAge(4800);
 		return resolver;
 	}
-	
+
 	@Bean
 	public void addInterceptor(InterceptorRegistry registry) {
 		LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
-		interceptor.setParamName("myLocale");
+		interceptor.setParamName("locale");
 		registry.addInterceptor(interceptor);
 	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+	/* DB configuration */
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("oracle.jdbc.OracleDriver");
+		dataSource.setUrl("jdbc:oracle:thin:@//localhost:1521/XE");
+		dataSource.setUsername("YALEXEYENKO");
+		dataSource.setPassword("019241218");
+		return dataSource;
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(dataSource());
+		JpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(jpaVendorAdapter);
+		em.setJpaProperties(additionalProperties());
+		return em;
+	}
+
+	private Properties additionalProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.Oracle10gDialect");
+		return properties;
+	}
+
+	@Bean
+	JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory);
+		return transactionManager;
+	}
+	
 
 }
---
+++ src/main/java/com/epam/yalexeyenko/config/Initializer.java
@@ -0,0 +1,38 @@
+package com.epam.yalexeyenko.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class Initializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+		ctx.register(AppConfiguration.class);
+		ctx.setServletContext(servletContext);
+		Dynamic dynamic = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+		dynamic.addMapping("/");
+		dynamic.setLoadOnStartup(1);
+	}
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] {AppConfiguration.class};
+	}
+	
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[]{"/*"};
+	}
+
+}
