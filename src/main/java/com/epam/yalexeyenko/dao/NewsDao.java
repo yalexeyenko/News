c033f8456bc8138b7cd86a65e1141c404456b04a
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-07-17 23:10:13 +0600

    Delete - Delete to implement Java based configuration.

--- src/main/java/com/epam/yalexeyenko/dao/NewsDao.java
+++
@@ -1,9 +0,0 @@
-package com.epam.yalexeyenko.dao;
-
-import java.util.List;
-
-import com.epam.yalexeyenko.model.News;
-
-public interface NewsDao extends Dao<News> {
-	List<News> findAllSortByDate();
-}
