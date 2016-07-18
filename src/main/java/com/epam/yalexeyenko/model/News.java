d4fab5d54c2298aa73be655e5dca7edc0e6866e4
Yevgeniy_Alexeyenko <Yevgeniy_Alexeyenko@.epam.com>
2016-07-18 18:06:50 +0600

    Delete - Delete named queries.

--- src/main/java/com/epam/yalexeyenko/model/News.java
+++ src/main/java/com/epam/yalexeyenko/model/News.java
@@ -4,18 +4,12 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity(name = "NEWSBLOCK")
 @Table(name = "NEWSBLOCK")
-@NamedQueries({
-	@NamedQuery(name = "News.findAll", query = "SELECT n FROM NEWSBLOCK n"),
-	@NamedQuery(name = "News.findAllOrderByDate", query = "SELECT n FROM NEWSBLOCK n ORDER BY n.date DESC")
-})
 public class News extends BaseEntity {
 	@Column(name = "TITLE")
 	private String title;
