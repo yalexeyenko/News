37cd88447e8049d4ac19ed4b6bf3d0fc55761511
Yevgeniy_Alexeyenko <Yevgeniy_Alexeyenko@.epam.com>
2016-07-18 18:06:23 +0600

    Add - Add annotation.

--- src/main/java/com/epam/yalexeyenko/repository/NewsRepository.java
+++ src/main/java/com/epam/yalexeyenko/repository/NewsRepository.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import com.epam.yalexeyenko.model.News;
 
+@Repository
 public interface NewsRepository extends JpaRepository<News, Integer> {
 	@Query("SELECT n FROM NEWSBLOCK n ORDER BY n.date DESC")
 	public List<News> findAllSortByDate();
