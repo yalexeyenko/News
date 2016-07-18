93dd4a5293ba751f17f9f4b1331586c2ad257c67
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-07-18 23:29:21 +0600

    Fix - Edit listNews() method.

--- src/main/java/com/epam/yalexeyenko/controller/NewsController.java
+++ src/main/java/com/epam/yalexeyenko/controller/NewsController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,13 +29,11 @@ public class NewsController {
 	private NewsService newsServiceImpl;
 	
 	@RequestMapping(value = "listNews")
-	public ModelAndView listNews() {
+	public ModelMap listNews(ModelMap modelMap) {
 		log.debug("listNews()...");
-		List<News> newsList = newsServiceImpl.findAllSortByDate();
-		ModelAndView modelAndView = new ModelAndView("listNews");
-		modelAndView.addObject("newsList", newsList);
-		modelAndView.addObject("newsCheckbox", new NewsCheckbox());
-		return modelAndView;
+		modelMap.addAttribute("newsList", newsServiceImpl.findAll());
+		modelMap.addAttribute("newsCheckbox", new NewsCheckbox());
+		return modelMap;
 	}
 
 	@RequestMapping(value = "showAddNews")
