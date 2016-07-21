34d5cbfd0337a3a7d33e42fd20275f1ac784b16f
Yevgeniy_Alexeyenko <Yevgeniy_Alexeyenko@.epam.com>
2016-07-21 12:47:55 +0600

    Refactor - Clean code.

--- src/main/java/com/epam/yalexeyenko/controller/NewsController.java
+++ src/main/java/com/epam/yalexeyenko/controller/NewsController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.epam.yalexeyenko.model.News;
 import com.epam.yalexeyenko.model.NewsCheckbox;
