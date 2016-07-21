f752709730478525397168824f43b326b94a951e
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-07-21 23:13:17 +0600

    Add - Add validation annotations.

--- src/main/java/com/epam/yalexeyenko/model/News.java
+++ src/main/java/com/epam/yalexeyenko/model/News.java
@@ -7,17 +7,31 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity(name = "BLOCKNEWS")
 @Table(name = "BLOCKNEWS")
 public class News extends BaseEntity {
+	@NotNull
+	@Size(min = 3, max = 100)
 	@Column(name = "TITLE")
 	private String title;
+	
+	@DateTimeFormat(pattern = "MM/dd/yyyy")
 	@Temporal(TemporalType.DATE)
 	@Column(name = "NEWSDATE")
 	private Date date;
+	
+	@NotNull
+	@Size(min = 3, max = 1000)
 	@Column(name = "BRIEF")
 	private String brief;
+	
+	@NotNull
+	@Size(min = 3, max = 2000)
 	@Column(name = "NEWSCONTENT")
 	private String content;
 
