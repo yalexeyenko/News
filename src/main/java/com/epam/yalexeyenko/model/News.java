a640535a7ddd801e0fb45b9858a7548944df378e
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-07-24 22:54:29 +0600

    Refactor - Change @NotNull annotation to @NotBlank.

--- src/main/java/com/epam/yalexeyenko/model/News.java
+++ src/main/java/com/epam/yalexeyenko/model/News.java
@@ -7,33 +7,31 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity(name = "BLOCKNEWS")
 @Table(name = "BLOCKNEWS")
 public class News extends BaseEntity {
-	@NotNull(message = "NotNull.news.title")
+	@NotBlank(message = "NotNull.news.title")
 	@Size(min = 3, max = 100, message = "Size.news.title")
 	@Column(name = "TITLE")
 	private String title;
 	
 	@DateTimeFormat(pattern = "MM/dd/yyyy")
-	@NotNull(message = "NotNull.news.date")
+	@NotBlank(message = "NotNull.news.date")
 	@Temporal(TemporalType.DATE)
 	@Column(name = "NEWSDATE")
 	private Date date;
 	
-	@NotNull(message = "NotNull.news.brief")
+	@NotBlank(message = "NotNull.news.brief")
 	@Size(min = 3, max = 1000, message = "Size.news.brief")
 	@Column(name = "BRIEF")
 	private String brief;
 	
-	@NotNull(message = "NotNull.news.content")
+	@NotBlank(message = "NotNull.news.content")
 	@Size(min = 3, max = 2000, message = "Size.news.content")
 	@Column(name = "NEWSCONTENT")
 	private String content;
