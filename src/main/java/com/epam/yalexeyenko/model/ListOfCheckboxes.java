072ec2c75ae028a99314e3dffc0fcf1156235d9a
Yevgeniy_Alexeyenko <Yevgeniy_Alexeyenko@.epam.com>
2016-07-25 14:59:38 +0600

    Refactor - Rename NewsCheckboxes class to ListOfCheckboxes.

---
+++ src/main/java/com/epam/yalexeyenko/model/ListOfCheckboxes.java
@@ -0,0 +1,19 @@
+package com.epam.yalexeyenko.model;
+
+import java.util.List;
+
+public class ListOfCheckboxes {
+	private List<Integer> idList;
+
+	public ListOfCheckboxes() {
+	}
+
+	public List<Integer> getIdList() {
+		return idList;
+	}
+
+	public void setIdList(List<Integer> idList) {
+		this.idList = idList;
+	}
+	
+}
