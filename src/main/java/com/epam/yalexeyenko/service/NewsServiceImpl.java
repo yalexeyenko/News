0a9b80822c402b0c7d5eb49efd713c69135111ef
Yevgeniy_Alexeyenko <Yevgeniy_Alexeyenko@.epam.com>
2016-07-25 16:06:49 +0600

    Refactor - Replace @Repository annotation to @Service.

--- src/main/java/com/epam/yalexeyenko/service/NewsServiceImpl.java
+++ src/main/java/com/epam/yalexeyenko/service/NewsServiceImpl.java
@@ -3,16 +3,15 @@ package com.epam.yalexeyenko.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.epam.yalexeyenko.model.News;
 import com.epam.yalexeyenko.repository.NewsRepository;
 
-@Repository
+@Service
 @Transactional
 public class NewsServiceImpl implements NewsService {
 	private static final Logger log = LoggerFactory.getLogger(NewsServiceImpl.class);
