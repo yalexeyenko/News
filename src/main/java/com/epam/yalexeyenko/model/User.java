0e2b7235c8f7d1954ab5f55b8032360b772d9158
Yevgeniy_Alexeyenko <Yevgeniy_Alexeyenko@.epam.com>
2016-07-27 17:28:05 +0600

    Add - Create User class;

---
+++ src/main/java/com/epam/yalexeyenko/model/User.java
@@ -0,0 +1,69 @@
+package com.epam.yalexeyenko.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USER")
+public class User extends BaseEntity {
+	@Column(name = "FIRSTNAME")
+	private String firstName;
+
+	@Column(name = "LASTNAME")
+	private String lastName;
+
+	@Column(name = "EMAIL")
+	private String email;
+
+	@Column(name = "PASSWORD")
+	private String password;
+	private String matchingPassword;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getMatchingPassword() {
+		return matchingPassword;
+	}
+
+	public void setMatchingPassword(String matchingPassword) {
+		this.matchingPassword = matchingPassword;
+	}
+	
+	public enum Role {
+		ADMIN,
+		USER,
+		GUEST
+	}
+
+}
