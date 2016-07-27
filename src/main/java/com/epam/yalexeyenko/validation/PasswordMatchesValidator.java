fd17bbe0487d77a044b2faced9fdaa99d4ec0598
Yevgeniy_Alexeyenko <Yevgeniy_Alexeyenko@.epam.com>
2016-07-27 17:29:23 +0600

    Add - Create PasswordMatchesValidator class;

---
+++ src/main/java/com/epam/yalexeyenko/validation/PasswordMatchesValidator.java
@@ -0,0 +1,20 @@
+package com.epam.yalexeyenko.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import dto.UserDTO;
+
+public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object> {
+
+	@Override
+	public void initialize(PasswordMatches arg0) {
+	}
+
+	@Override
+	public boolean isValid(Object obj, ConstraintValidatorContext context) {
+		UserDTO userDTO = (UserDTO) obj;
+		return userDTO.getPassword().equals(userDTO.getMatchingPassword());
+	}
+
+}
