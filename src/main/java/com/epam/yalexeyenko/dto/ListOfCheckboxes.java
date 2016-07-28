242f7be314a9ece85af4c462193de921a1bd823a
Yevgeniy_Alexeyenko <Yevgeniy_Alexeyenko@.epam.com>
2016-07-28 17:43:59 +0600

    Add - Create ListOfCheckBoxes class.

---
+++ src/main/java/com/epam/yalexeyenko/dto/ListOfCheckboxes.java
@@ -0,0 +1,19 @@
+package com.epam.yalexeyenko.dto;
+
+import java.util.List;
+
+public class ListOfCheckboxes {
+	private List<Long> idList;
+
+	public ListOfCheckboxes() {
+	}
+
+	public List<Long> getIdList() {
+		return idList;
+	}
+
+	public void setIdList(List<Long> idList) {
+		this.idList = idList;
+	}
+	
+}
