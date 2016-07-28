ba38fb28589ecbaab89ad69a5e05d53ab13a3065
Yevgeniy_Alexeyenko <Yevgeniy_Alexeyenko@.epam.com>
2016-07-28 17:43:08 +0600

    Add - Create MessageSecurityWebApplicationInitializer class.

---
+++ src/main/java/com/epam/yalexeyenko/config/MessageSecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.epam.yalexeyenko.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MessageSecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
