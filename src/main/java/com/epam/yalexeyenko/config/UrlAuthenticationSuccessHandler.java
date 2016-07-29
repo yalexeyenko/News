ec83afa3d42cdad9aa83e901d285033c0c12fe8b
Yevgeniy_Alexeyenko <Yevgeniy_Alexeyenko@.epam.com>
2016-07-29 17:12:36 +0600

    Add - Create UrlAuthenticationSuccessHandler class.

---
+++ src/main/java/com/epam/yalexeyenko/config/UrlAuthenticationSuccessHandler.java
@@ -0,0 +1,92 @@
+package com.epam.yalexeyenko.config;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import com.epam.yalexeyenko.controller.NewsController;
+
+@Component
+public class UrlAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+	private static final Logger log = LoggerFactory.getLogger(UrlAuthenticationSuccessHandler.class);
+	
+	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+	
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		handle(request, response, authentication);
+		clearAuthenticationAttributes(request);
+	}
+	
+	protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+			throws IOException, ServletException {
+		String targetUrl = determineTargetUrl(authentication);
+		
+		if (response.isCommitted()) {
+			log.debug("Response has already neen committed. Unable to redirect to " + targetUrl);
+			return;
+		}
+		redirectStrategy.sendRedirect(request, response, targetUrl);
+	}
+	
+	protected String determineTargetUrl(Authentication authentication) {
+		boolean isUser = false;
+		boolean isAdmin = false;
+		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+		log.debug("authorities.size():", authorities.size());
+		log.debug("authorities:", authorities);
+		for (GrantedAuthority grantedAuthority : authorities) {
+			log.debug("grantedAuthority.getAuthority(): {}", grantedAuthority.getAuthority());
+			if (grantedAuthority.getAuthority().equals("ROLE_USER")) {
+				isUser = true;
+				break;
+			} else if (grantedAuthority.getAuthority().equals("ROLE_ADMIN")) {
+				isAdmin = true;
+				break;
+			}
+		}
+		log.debug("isUser(): {}", isUser);
+		if (isUser) {
+			return "/cabinet";
+		} else if (isAdmin) {
+			return "/admin";
+		} else {
+			return "/home";
+		}
+	}
+	
+	protected void clearAuthenticationAttributes(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		if (session == null) {
+			return;
+		}
+		session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+	}
+
+	public RedirectStrategy getRedirectStrategy() {
+		return redirectStrategy;
+	}
+
+	public void setRedirectStrategy(RedirectStrategy redirectStrategy) {
+		this.redirectStrategy = redirectStrategy;
+	}
+
+}
