ffe7c88ee65abb0d42a114ecc020511324f6c2fd
Yevgeniy_Alexeyenko <Yevgeniy_Alexeyenko@.epam.com>
2016-08-01 17:25:48 +0600

    Add - Create Role class.

---
+++ src/main/java/com/epam/yalexeyenko/model/Role.java
@@ -0,0 +1,33 @@
+package com.epam.yalexeyenko.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ROLE")
+public class Role extends BaseEntity {
+	@Column(name = "NAME")
+	private String name;
+	@ManyToMany(mappedBy = "roles")
+	private List<User> users;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+}
