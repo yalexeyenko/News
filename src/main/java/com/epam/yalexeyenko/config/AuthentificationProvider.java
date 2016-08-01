6cae5456f55ebfaf0757f58e35ed6a218bf0ff1a
Yevgeniy_Alexeyenko <Yevgeniy_Alexeyenko@.epam.com>
2016-08-01 17:27:01 +0600

    Add - Create AuthentificationProvider interface.

---
+++ src/main/java/com/epam/yalexeyenko/config/AuthentificationProvider.java
@@ -0,0 +1,6 @@
+package com.epam.yalexeyenko.config;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface AuthentificationProvider extends UserDetailsService {
+}
