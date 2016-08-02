eae9d85552837b42beecf67992c2db3a8340e7b1
Yevgeniy_Alexeyenko <Yevgeniy_Alexeyenko@.epam.com>
2016-08-02 17:56:24 +0600

    Add - Create user, status fields. Its getters and setters.

--- src/main/java/com/epam/yalexeyenko/model/News.java
+++ src/main/java/com/epam/yalexeyenko/model/News.java
@@ -5,6 +5,9 @@ import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Convert;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.epam.yalexeyenko.converter.LocalDateAttributeConverter;
@@ -25,6 +28,18 @@ public class News extends BaseEntity {
 	
 	@Column(name = "NEWSCONTENT")
 	private String content;
+	
+//	@ManyToOne(fetch = FetchType.LAZY)
+//	@JoinColumn(name = "ID", insertable=false, updatable=false)
+	@ManyToOne
+	@JoinColumn(name = "STATUS_ID")
+	private Status status;
+	
+//	@ManyToOne(fetch = FetchType.LAZY)
+//	@JoinColumn(name = "ID", insertable=false, updatable=false)
+	@ManyToOne()
+	@JoinColumn(name = "USER_ID")
+	private User user;
 
 	public News() {
 	}
@@ -59,6 +74,22 @@ public class News extends BaseEntity {
 
 	public void setContent(String content) {
 		this.content = content;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
 	}
 
 }
