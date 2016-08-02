026f0851b3cd01b52ad0a869f785e3536a21fd78
Yevgeniy_Alexeyenko <Yevgeniy_Alexeyenko@.epam.com>
2016-08-02 17:53:58 +0600

    Add - Create FindAllByUser() method.

--- src/main/java/com/epam/yalexeyenko/service/NewsService.java
+++ src/main/java/com/epam/yalexeyenko/service/NewsService.java
@@ -4,7 +4,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.epam.yalexeyenko.dto.NewsDTO;
+import com.epam.yalexeyenko.model.User;
 
 public interface NewsService extends Service<NewsDTO> {
 	Page<NewsDTO> findAll(Pageable pageRequest);
+	NewsDTO create(NewsDTO newsDTO, String email);
+	Page<NewsDTO> findAllByUser(Pageable pageRequest, String userEmail);
 }
