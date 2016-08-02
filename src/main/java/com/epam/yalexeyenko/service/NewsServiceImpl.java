a4696afdab3cdda142c46012c5c563bc8d268b69
Yevgeniy_Alexeyenko <Yevgeniy_Alexeyenko@.epam.com>
2016-08-02 17:53:39 +0600

    Add - Create findByUser() method.

--- src/main/java/com/epam/yalexeyenko/service/NewsServiceImpl.java
+++ src/main/java/com/epam/yalexeyenko/service/NewsServiceImpl.java
@@ -2,23 +2,37 @@ package com.epam.yalexeyenko.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.epam.yalexeyenko.controller.NewsController;
 import com.epam.yalexeyenko.converter.NewsConverter;
 import com.epam.yalexeyenko.dto.NewsDTO;
 import com.epam.yalexeyenko.model.News;
 import com.epam.yalexeyenko.repository.NewsRepository;
+import com.epam.yalexeyenko.repository.StatusRepository;
+import com.epam.yalexeyenko.repository.UserRepository;
 
 @Service
 @Transactional
 public class NewsServiceImpl implements NewsService {
+	private static final Logger log = LoggerFactory.getLogger(NewsServiceImpl.class);
+	
 	@Autowired
 	private NewsRepository newsRepository;
+	
+	@Autowired
+	private StatusRepository statusRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
 	
 	@Autowired
 	private NewsConverter newsConverter;
@@ -40,8 +54,10 @@ public class NewsServiceImpl implements NewsService {
 	}
 
 	@Override
-	public NewsDTO create(NewsDTO newsDTO) {
+	public NewsDTO create(NewsDTO newsDTO, String email) {
 		News news = newsConverter.newsDTOToNews(newsDTO);
+		news.setStatus(statusRepository.findByName("DRAFT"));
+		news.setUser(userRepository.findByEmail(email));
 		return newsConverter.newsToDTO(newsRepository.saveAndFlush(news));
 	}
 
@@ -53,6 +69,14 @@ public class NewsServiceImpl implements NewsService {
 	@Override
 	public Page<NewsDTO> findAll(Pageable pageRequest) {
 		Page<News> pageNews = newsRepository.findAll(pageRequest);
+		log.debug("pageNews.getContent().get(0).getUser().getEmail(): {}", pageNews.getContent().get(0).getUser().getEmail());
+		return new PageImpl<NewsDTO>(newsConverter.newsToDTO(pageNews.getContent()), pageRequest, pageNews.getTotalElements());
+	}
+	
+	@Override
+	public Page<NewsDTO> findAllByUser(Pageable pageRequest, String userEmail) {
+		Page<News> pageNews = newsRepository.findByUser(pageRequest, userRepository.findByEmail(userEmail));
+		log.debug("pageNews.getContent().size(): {}", pageNews.getContent().size());
 		return new PageImpl<NewsDTO>(newsConverter.newsToDTO(pageNews.getContent()), pageRequest, pageNews.getTotalElements());
 	}
 
@@ -69,5 +93,10 @@ public class NewsServiceImpl implements NewsService {
 	@Override
 	public List<NewsDTO> findAll() {
 		return newsConverter.newsToDTO(newsRepository.findAll());
+	}
+	
+	@Override
+	public NewsDTO create(NewsDTO t) {
+		throw new UnsupportedOperationException("Use create(NewsDTO newsDTO, String email) method to create news.");
 	}
 }
