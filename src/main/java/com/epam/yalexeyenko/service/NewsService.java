357f5f8f652a985db55df392e1eccde3dbac64bb
Yevgeniy_Alexeyenko <Yevgeniy_Alexeyenko@.epam.com>
2016-08-03 17:41:33 +0600

    Add - Create findByDateBetween() method.

--- src/main/java/com/epam/yalexeyenko/service/NewsService.java
+++ src/main/java/com/epam/yalexeyenko/service/NewsService.java
@@ -1,14 +1,16 @@
 package com.epam.yalexeyenko.service;
 
+import java.time.LocalDate;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.epam.yalexeyenko.dto.NewsDTO;
-import com.epam.yalexeyenko.model.User;
 
 public interface NewsService extends Service<NewsDTO> {
 	Page<NewsDTO> findAll(Pageable pageRequest);
 	NewsDTO create(NewsDTO newsDTO, String email);
 	void update(NewsDTO newsDTO, String email);
 	Page<NewsDTO> findAllByUser(Pageable pageRequest, String userEmail);
+	Page<NewsDTO> findByDateBetween(Pageable pageRequest, LocalDate start, LocalDate end, String userEmail);
 }
