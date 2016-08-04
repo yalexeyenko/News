1e6ae3157e071c161c6745aa2e84ed31113483f3
Yevgeniy_Alexeyenko <Yevgeniy_Alexeyenko@.epam.com>
2016-08-04 17:44:46 +0600

    Add - Create findByStatus() method.

--- src/main/java/com/epam/yalexeyenko/repository/NewsRepository.java
+++ src/main/java/com/epam/yalexeyenko/repository/NewsRepository.java
@@ -8,11 +8,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.epam.yalexeyenko.model.News;
+import com.epam.yalexeyenko.model.Status;
 import com.epam.yalexeyenko.model.User;
 
 @Repository
 public interface NewsRepository extends JpaRepository<News, Long> {
 	Page<News> findAll(Pageable pageable);
 	Page<News> findByUser(Pageable pageable, User user);
+	Page<News> findByStatus(Pageable pageable, Status status);
 	Page<News> findByDateBetweenAndUser(Pageable pageable, LocalDate start, LocalDate end, User user);
 }
