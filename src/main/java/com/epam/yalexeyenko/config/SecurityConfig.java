e12a49b89d263e69c29cf2eb635bd160afda2e0e
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-08-06 20:30:42 +0600

    Add - @EnableGlobalMethodSecurity, @ComponentScan annotations.

--- src/main/java/com/epam/yalexeyenko/config/SecurityConfig.java
+++ src/main/java/com/epam/yalexeyenko/config/SecurityConfig.java
@@ -2,9 +2,11 @@ package com.epam.yalexeyenko.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,6 +16,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@ComponentScan(basePackages = {"com.epam.yalexeyenko"})
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
