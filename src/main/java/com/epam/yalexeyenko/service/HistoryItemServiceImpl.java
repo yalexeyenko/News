04fa477f249a1fe3a8d0a0c875625659dc6a6d34
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-08-07 23:39:03 +0600

    Add - Create HistoryItemServiceImpl class.

---
+++ src/main/java/com/epam/yalexeyenko/service/HistoryItemServiceImpl.java
@@ -0,0 +1,59 @@
+package com.epam.yalexeyenko.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.epam.yalexeyenko.model.HistoryItem;
+import com.epam.yalexeyenko.repository.HistoryItemRepository;
+
+@Service
+@Transactional
+public class HistoryItemServiceImpl implements HistoryItemService {
+
+	@Autowired
+	private HistoryItemRepository historyItemRepository;
+
+	@Override
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public HistoryItem create(HistoryItem t) {
+		throw new RuntimeException("RuntimeException inside create()");
+//		return historyItemRepository.saveAndFlush(t);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public HistoryItem find(Long id) {
+		return historyItemRepository.findOne(id);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Page<HistoryItem> findAll(Pageable pageRequest) {
+		return historyItemRepository.findAll(pageRequest);
+	}
+
+	@Override
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public void update(HistoryItem t) {
+		historyItemRepository.saveAndFlush(t);
+	}
+
+	@Override
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public void delete(Long id) {
+		historyItemRepository.delete(id);
+	}
+
+	public HistoryItemRepository getHistoryItemRepository() {
+		return historyItemRepository;
+	}
+
+	public void setHistoryItemRepository(HistoryItemRepository historyItemRepository) {
+		this.historyItemRepository = historyItemRepository;
+	}
+
+}
