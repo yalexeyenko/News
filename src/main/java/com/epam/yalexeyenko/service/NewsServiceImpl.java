9176ba777c5128225c73e91ae04b72f0f08c693a
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-08-07 17:26:50 +0600

    Refactor - Organize imports.

--- src/main/java/com/epam/yalexeyenko/service/NewsServiceImpl.java
+++ src/main/java/com/epam/yalexeyenko/service/NewsServiceImpl.java
@@ -6,14 +6,12 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.epam.yalexeyenko.controller.NewsController;
 import com.epam.yalexeyenko.converter.NewsConverter;
 import com.epam.yalexeyenko.dto.NewsDTO;
 import com.epam.yalexeyenko.model.News;
